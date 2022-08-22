f3d42f2f9cb66e71813ba793b90647350345468f
minsoo-s <108928895+minsoo-s@users.noreply.github.com>
2022-08-22 22:53:18 +0900

    0822_teamp

---
+++ shiny/pub_shinyStudy/app.R
@@ -0,0 +1,14 @@
+library(shiny)
+library(ggplot2)
+library(gapminder)
+
+ui <- fluidPage(
+plotOutput("plot", click = "plot_click"),
+tableOutput("data")
+)
+
+server<-function(input,output){
+    output$plot <-renderplot({
+        
+    })
+}
